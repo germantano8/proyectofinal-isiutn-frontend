d2ee861a663b5d18d589fb1d2ed76463fdb3d6a5
germantano8 <germantano88@gmail.com>
2024-01-11 19:15:20 -0300

    init

---
+++ src/pages/Login.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+function App() {
+	const [nombre, setNombre] = useState('')
+	const [password, setPassword] = useState('')
+
+	async function loginUser(e) {
+		e.preventDefault()
+
+		const response = await fetch('http://localhost:3000/api/usuario/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			// credentials:'include',
+			body: JSON.stringify({
+				nombre,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+
+        console.log(data)
+
+        if (data.token) {
+			localStorage.setItem('token', data.token)
+        }else{
+			alert("mal ingresados")
+		}
+
+		const data2 = await fetch('http://localhost:3000/api/cliente', {
+			method: 'GET'})
+
+		console.log(data2)
+	}
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<form onSubmit={loginUser}>
+				<input
+					value={nombre}
+					onChange={(e) => setNombre(e.target.value)}
+					type="text"
+				/>
+				<br />
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+				/>
+				<br />
+				<input type="submit" value="Login" />
+			</form>
+		</div>
+	)
+}
+
+export default App
