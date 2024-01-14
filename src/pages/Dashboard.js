fc03eb4088a7d71a36bbc9f77433eb65c03308a2
germantano8 <germantano88@gmail.com>
2024-01-14 10:52:21 -0300

    token

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -9,23 +9,24 @@ const Dashboard = () => {
         const token = cookies.get('token')
         
         if(token){
-            cookies.remove('token')
+            const response2 = await fetch('http://localhost:3000/api/vehiculo',{
+            method:'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials:'include',
+            })
+
+            const data2 = await response2.json()
+            console.log(data2)
+        }else{
+            console.log("no estás logueado perro")
         }
-
-        const response2 = await fetch('http://localhost:3000/api/vehiculo',{
-        method:'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials:'include',
-        })
-
-        const data2 = await response2.json()
-        console.log(data2)
+        
     }
 
     return (
-    <button type="submit" onClick={req}>holsa</button>
+        <button type="submit" onClick={req}>holsa</button>
     )
 }
 
