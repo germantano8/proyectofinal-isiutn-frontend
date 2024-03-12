1863d7060d00b22b09e567e28fee2eabcf8f0fd7
Germán Tano <germantano88@gmail.com>
2024-03-12 20:15:29 -0300

    abm trabajos, clientes y rutas protegidas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import {cookies} from './utils/utils'
 import Login from './pages/Login/Login'
-import Dasboard from './pages/Dashboard/Dashboard'
+import Dashboard from './pages/Dashboard/Dashboard'
 import Clientes from './pages/Clientes/Clientes'
 import Trabajos from './pages/Trabajos/Trabajos'
 
 const App = () => {
+
+  const token = cookies.get('token')
+
 	return (
     <div className='container'>
       <br/>
       <div className="row justify-content-center">
         <Routes>
-          <Route path="/" element={<Dasboard/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/trabajos" element={<Trabajos/>} />
-          <Route path="/clientes" element={<Clientes/>} />
+          <Route path="/login" element={<Login />} />
+          {token ? (
+            <>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/trabajos" element={<Trabajos />} />
+              <Route path="/clientes" element={<Clientes />} />
+            </>
+          ) : 
+            <Route path='*' element={<Navigate to='/login'></Navigate>}></Route>
+          }
         </Routes>
       </div>
     </div>
