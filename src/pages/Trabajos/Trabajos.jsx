7e7014b5a70140aa25fd60928709c77b6253ae19
Candela Poratti <cande.poratti@gmail.com>
2024-04-24 14:08:26 -0300

    trabajos fixed

--- src/pages/Trabajos/Trabajos.jsx
+++ src/pages/Trabajos/Trabajos.jsx
@@ -1,10 +1,28 @@
-import {React} from 'react'
+import {React, useEffect, useState} from 'react'
 import { deleteData, useGetData } from '../../hooks';
 import ModalFormularioTrabajo from '../../components/ModalFormularioTrabajo'
 import {Loading} from '../../components/'
 
 const Trabajos = () => {
-    const [trabajos, loading] = useGetData('trabajo');
+    const [trabajos, loadingTrabajos] = useGetData('trabajo');
+    const [clientes] = useGetData('cliente'); // Suponiendo que 'cliente' es la ruta correcta
+    const [proyectos] = useGetData('proyecto'); // Suponiendo que 'proyecto' es la ruta correcta
+    const [clienteMap, setClienteMap] = useState({});
+    const [proyectoMap, setProyectoMap] = useState({});
+
+    useEffect(() => {
+        const clienteMap = {};
+        clientes.forEach(cliente => {
+            clienteMap[cliente.cuit] = cliente.razon_social;
+        });
+        setClienteMap(clienteMap);
+
+        const proyectoMap = {};
+        proyectos.forEach(proyecto => {
+            proyectoMap[proyecto.id] = proyecto.nombre;
+        });
+        setProyectoMap(proyectoMap);
+    }, [clientes, proyectos]);
 
     // Acá se define la estructura del objeto que vamos a estar trabajando en esta página
     const props={
@@ -28,11 +46,6 @@ const Trabajos = () => {
         <div className='table-responsive col-12 col-md-6 col-lg-9'>
             <h2 className='text-left'>Trabajos</h2>
 
-            {/* Acá se llama al componente ModalFormulario, que se encarga de mostrar un modal con un formulario para agregar un nuevo cliente 
-                element: el nombre del elemento que se va a agregar (va a servir para posteriormente hacer los fetch a la URL correspondientes), 
-                value: el texto que se va a mostrar en el botón (va a variar según la página en la que estemos trabajando), 
-                props: las propiedades del objeto que se va a agregar
-            */}
             <ModalFormularioTrabajo value={"Nuevo trabajo"} props={props} mode={'new'}/>
 
             <table className="table table-striped">
@@ -44,28 +57,30 @@ const Trabajos = () => {
                     <th scope="col">Fecha hasta</th>
                     <th scope="col">Kilometraje</th>
                     <th scope="col">Tipo trabajo</th>
+                    <th scope="col">Cliente/Proyecto</th> {/* Añade esta línea */}
                 </tr>
                 </thead>
 
-                {loading && <Loading/>}
+                {loadingTrabajos && <Loading/>}
 
                 <tbody>
                     {
                     trabajos.map((t) => {
                         return (
-                        <tr key={t.id_trabajo}>
-                            <td>{t.id_trabajo}</td>
-                            <td>{t.patente}</td>
-                            <td>{t.fecha_desde}</td>
-                            <td>{t.fecha_hasta}</td>
-                            <td>{t.kilometraje}</td>
-                            <td>{t.cuit_cliente === '11111111111' ? "Propio" : "Alquiler"}</td>
-                            <td>
-                                <ModalFormularioTrabajo value={<i class="bi bi-pencil"></i>} props={trabajos.find((objeto) => Object.values(objeto)[0] === t.id_trabajo)} mode={'update'} id={t.id_trabajo}/>
-                                &ensp;
-                                <button className='btn btn-danger' onClick={() => {deleteItem(t.id_trabajo)}}><i class="bi bi-trash3"></i></button>
-                            </td>
-                        </tr>
+                            <tr key={t.id_trabajo}>
+                                <td>{t.id_trabajo}</td>
+                                <td>{t.patente}</td>
+                                <td>{t.fecha_desde}</td>
+                                <td>{t.fecha_hasta}</td>
+                                <td>{t.kilometraje}</td>
+                                <td>{t.cuit_cliente === '00000000000' ? "Propio" : "Alquiler"}</td>
+                                <td>{t.cuit_cliente === '00000000000' ? "Proyecto: " + proyectoMap[t.id_proyecto] : clienteMap[t.cuit_cliente]}</td> 
+                                <td>
+                                    <ModalFormularioTrabajo value={<i class="bi bi-pencil"></i>} props={trabajos.find((objeto) => Object.values(objeto)[0] === t.id_trabajo)} mode={'update'} id={t.id_trabajo}/>
+                                    &ensp;
+                                    <button className='btn btn-danger' onClick={() => {deleteItem(t.id_trabajo)}}><i class="bi bi-trash3"></i></button>
+                                </td>
+                            </tr>
                         )
                     })
                     }
