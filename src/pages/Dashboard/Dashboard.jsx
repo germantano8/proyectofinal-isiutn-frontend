c93c783a32230f985f915969ae45ce447a9692ac
Candela Poratti <cande.poratti@gmail.com>
2024-04-25 16:33:44 -0300

    dashboards finished

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,9 @@
-import Cards from '../../components/Cards'
-
 const Dashboard = () => {
 
     return (
         <div className="col-12 col-md-6 col-lg-9">
-            <Cards/>
+            <h1 className='text-left'>Panel general</h1>
+            <iframe title="overview" width="1040" height="640" src="https://app.powerbi.com/reportEmbed?reportId=a25a8efc-5a69-4663-82be-d22087010b70&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
         </div>
     )
 }
