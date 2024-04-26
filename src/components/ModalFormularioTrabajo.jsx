8d3e18c32049d7b1d1e168418f217c166563e123
Germán Tano <81445495+germantano8@users.noreply.github.com>
2024-04-26 19:11:30 -0300

    Develop (#10)
    * login iniciado
    
    * proban2
    
    * autenticación inciada
    
    * probando metodo get
    
    * init login y register
    
    * token
    
    * login funcional
    
    * funciona login - dashboard empezado
    
    * dashboard cards iniciado
    
    * init vehiculos
    - hay que hacer componente loading y mover el useEffect
    
    * cards incializadas
    
    * hechas cards
    
    * init clientes
    
    * client management started
    
    * crud clientes iniciado
    
    * delete request clientes
    
    * modal pending
    
    * insert done, edit left
    
    * Clientes insert - edit pending
    
    * update data done, validate forms pending
    
    * trabajos abm
    
    * validaciones cliente, trabajo no anda
    
    * abm trabajos, clientes y rutas protegidas
    
    * Proyectos ger (#4)
    
    * Completada página de proyectos y trabajos, refactor de imports, validaciones varias
    
    * reorden dos botones
    
    * ABM proyectos y trabajos. Validaciones proyectos, trabajos y login (#5)
    
    * haciendo nuevo trabajo
    
    * insertar y actualizar trabajos
    
    * ABM proyectos y trabajos, validaciones de esos + login
    
    * minor fixes
    
    * trabajos  (#6)
    
    * trabajos extraido de proyecto
    
    * trabajo extraído de proyecto
    
    * minor fixes
    
    * trabajos funcionando
    
    * fix modalform trabajo
    
    ---------
    
    Co-authored-by: Candela Poratti <cande.poratti@gmail.com>
    
    * Abm reparaciones, service, vehiculos (#7)
    
    * modif public tab
    
    * reparaciones- sin modificacion
    
    * Métricas / pwbi embebido (ejemplo)
    
    * reparaciones fixes
    
    * abm vehiculos
    
    * abm service
    
    * vehiculos sn funcionar
    
    * minor fixes
    
    * minor fixes
    
    ---------
    
    Co-authored-by: Candela Poratti <cande.poratti@gmail.com>
    
    * fix runtime errors
    
    * minor fix
    
    * trabajo almost fixed
    
    * sidebar
    
    * Dashboards (#9)
    
    * acordeon service
    
    * trabajos fixed
    
    * visual mods
    
    * dashboards finished
    
    ---------
    
    Co-authored-by: Candela Poratti <cande.poratti@gmail.com>
    
    ---------
    
    Co-authored-by: Candela Poratti <cande.poratti@gmail.com>

--- src/components/ModalFormularioTrabajo.jsx
+++ src/components/ModalFormularioTrabajo.jsx
@@ -8,15 +8,10 @@ const ModalFormularioTrabajo = ({ value, props, mode, id }) => {
     const clientes = useGetData('cliente');
     const vehiculos = useGetData('vehiculo');
     const proyectos = useGetData('proyecto');
-    // vehiculos = vehiculos.filter((v) => v.estado === 'Disponible');
-    const [formData, setFormData] = useState({
-        ...props,
-    });
 
     const [formData, setFormData] = useState({ ...props });
     const [errors, setErrors] = useState([]);
     const [show, setShow] = useState(false);
-    const [tipoTrabajo, setTipoTrabajo] = useState('');
 
     const handleShow = () => setShow(true);
     const handleClose = () => {
@@ -36,7 +31,8 @@ const ModalFormularioTrabajo = ({ value, props, mode, id }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try{
-            let isValid = await trabajoSchema.validate(formData, { abortEarly: false });
+            let isValid 
+            isValid = await trabajoSchema.validate(formData, { abortEarly: false });
             if(isValid){
                 if(mode==='new'){
                     await insertData('trabajo', formData);
@@ -53,16 +49,6 @@ const ModalFormularioTrabajo = ({ value, props, mode, id }) => {
         }
     };
 
-    const handleTipoTrabajoChange = (e) => {
-        setTipoTrabajo(e.target.value);
-        // Resetear el campo correspondiente al cambiar el tipo de trabajo
-        setFormData({
-            ...formData,
-            id_proyecto: '',
-            cuit_cliente: '',
-        });
-    };
-
     return (
         <>
             <Button className="btn btn-orange" onClick={handleShow}>
@@ -75,57 +61,6 @@ const ModalFormularioTrabajo = ({ value, props, mode, id }) => {
                 </Modal.Header>
                 <Modal.Body>
                     <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3">
-                            <Form.Check
-                                type="radio"
-                                label="Propio"
-                                name="tipoTrabajo"
-                                value="PROPIO"
-                                checked={tipoTrabajo === 'PROPIO'}
-                                onChange={handleTipoTrabajoChange}
-                            />
-                            <Form.Check
-                                type="radio"
-                                label="Alquiler"
-                                name="tipoTrabajo"
-                                value="ALQUILER"
-                                checked={tipoTrabajo === 'ALQUILER'}
-                                onChange={handleTipoTrabajoChange}
-                            />
-                        </Form.Group>
-
-                        {tipoTrabajo === 'PROPIO' && (
-                            <Form.Group className="mb-3" name="id_proyecto">
-                                <Form.Label>PROYECTO</Form.Label>
-                                <Form.Control
-                                    type="number"
-                                    placeholder="ID PROYECTO"
-                                    value={formData['id_proyecto']}
-                                    name="id_proyecto"
-                                    onChange={handleChange}
-                                    disabled={!tipoTrabajo}
-                                />
-                            </Form.Group>
-                        )}
-
-                        {tipoTrabajo === 'ALQUILER' && (
-                            <Form.Group className="mb-3" name="cuit_cliente">
-                                <Form.Label>CLIENTE</Form.Label>
-                                <Form.Select
-                                    title="Seleccionar cliente"
-                                    onChange={handleChange}
-                                    value={formData.cuit_cliente}
-                                    name="cuit_cliente"
-                                    disabled={!tipoTrabajo}
-                                >
-                                    {clientes[0].map((c) => (
-                                        <option key={c.cuit} value={c.cuit}>
-                                            {c.cuit} - {c.razon_social}
-                                        </option>
-                                    ))}
-                                </Form.Select>
-                            </Form.Group>
-                        )}
 
                        <Form.Group className="mb-3" name="id_trabajo">
                         <Form.Label>ID TRABAJO</Form.Label>
@@ -163,48 +98,76 @@ const ModalFormularioTrabajo = ({ value, props, mode, id }) => {
                             name={'kilometraje'}
                             onChange={(e)=>{handleChange(e, 'kilometraje')}} />
                     </Form.Group>
-
-
+            
                     <Form.Group className="mb-3" name="patente">
-                        <Form.Label>PATENTE</Form.Label>
+                         <Form.Label>PATENTE DEL VEHÍCULO</Form.Label>
                         <Form.Select
                             title="Seleccionar patente"
-                            onChange={(e)=>{handleChange(e, 'patente')}}
-                            value={formData.patente}>
+                            name={'patente'}
+                            onChange={(e) => { handleChange(e, 'patente') }}
+                            value={formData.patente}
+                        >
+                            <option value="" disabled hidden>Seleccionar</option>
                             {vehiculos[0].map((v) => (
                                 <option key={v.patente} value={v.patente}>{v.patente}</option>
                             ))}
                         </Form.Select>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" name="id_proyecto">
-                        <Form.Label>ID PROYECTO</Form.Label>
-                        <Form.Select
-                            placeholder="ID PROYECTO"
-                            value={formData['id_proyecto']}
-                            name={'id_proyecto'}
-                            onChange={(e)=>{handleChange(e, 'id_proyecto')}}>
-                        {proyectos[0].map((p) => (
-                            <option key={p.id} value={p.id}>
-                                {p.id} - {p.nombre} 
-                            </option>
-                        ))}
-                        </Form.Select>
-                    </Form.Group>
-
                     <Form.Group className="mb-3" name="dni_conductor">
-                        <Form.Label>CONDUCTOR</Form.Label>
+                         <Form.Label>CONDUCTOR</Form.Label>
                         <Form.Select
                             title="Seleccionar conductor"
-                            onChange={(e)=>{handleChange(e, 'dni_conductor')}}
-                            value={formData.dni_conductor}>
-                            {conductores[0].map((c) => (
-                                <option key={c.dni} value={c.dni}>
-                                    {c.nombre} {c.apellido}
-                                </option>
+                            name={'dni_conductor'}
+                            onChange={(e) => { handleChange(e, 'dni_conductor') }}
+                            value={formData.dni_conductor}
+                        >
+                            <option value="" disabled hidden>Seleccionar</option>
+                            {conductores[0].map((v) => (
+                                <option key={v.dni} value={v.dni}>{v.nombre} {v.apellido}</option>
                             ))}
                         </Form.Select>
                     </Form.Group>
+
+                    <Form.Group className="mb-3" name="cuit_cliente">
+                         <Form.Label>CLIENTE</Form.Label>
+                        <Form.Select
+                            title="Seleccionar proyecto"
+                            name={'cuit_cliente'}
+                            onChange={(e) => { handleChange(e, 'cuit_cliente') }}
+                            value={formData.cuit_cliente}
+                        >
+                            <option value="" disabled hidden>Seleccionar</option>
+                            {clientes[0].map((v) => (
+                                <option key={v.cuit} value={v.cuit}>{v.razon_social}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" name="id_proyecto">
+                         <Form.Label>PROYECTO</Form.Label>
+                        <Form.Select
+                            title="Seleccionar conductor"
+                            name={'id_proyecto'}
+                            onChange={(e) => { handleChange(e, 'id_proyecto') }}
+                            value={formData.id_proyecto}
+                        >
+                            <option value="" disabled hidden>Seleccionar</option>
+                            {proyectos[0].map((v) => (
+                                <option key={v.id} value={v.id}>{v.nombre}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+
+                    {errors.length>0 &&
+                        <div className="alert alert-danger">
+                            { errors.map((e) => (
+                                    <div>
+                                        {e}
+                                    </div>
+                                ))}
+                        </div>
+                    } 
 		
                         <Button variant="secondary" onClick={handleClose}>
                             Cerrar
--- src/pages/Metricas/Metricas.jsx
+++ src/pages/Metricas/Metricas.jsx
@@ -6,9 +6,7 @@ const Metricas = () => {
     <>      
         <div className='table-responsive col-12 col-md-6 col-lg-9'>
           <h1 className='text-left'>Métricas de uso</h1>
-          <iframe title="Sales & Returns Sample v201912" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=727d2b95-e887-4511-9814-e12aae70d6ae&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
-        {/* La linea de arriba se obtiene desde el archivo PWBI (versiòn online, archivo publicado). 
-        Cambiarla luego por el PWBI definitivo una vez construido el mismo */}
+            <iframe title="metricas" width="1020" height="600" src="https://app.powerbi.com/reportEmbed?reportId=a0e09203-b40d-4989-9878-808447aedbb0&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
         </div>
     </>
     )
