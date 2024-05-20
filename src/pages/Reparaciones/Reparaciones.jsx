1fa239d2ae2bb54984f977716154670b4ced3016
Candela Poratti <cande.poratti@gmail.com>
2024-05-20 20:14:49 -0300

    FIXED: detalle-filtros desde vehiculos

--- src/pages/Reparaciones/Reparaciones.jsx
+++ src/pages/Reparaciones/Reparaciones.jsx
@@ -1,124 +1,111 @@
-import {React, useState} from 'react'
-import {useGetData} from '../../hooks/getData'
-import Loading from '../../components/Loading'
-import EditDelete from '../../components/EditDelete'
-import ModalFormularioReparacion from '../../components/ModalFormularioReparacion'
+import React, { useState } from 'react';
+import { useGetData } from '../../hooks/getData';
+import Loading from '../../components/Loading';
+import EditDelete from '../../components/EditDelete';
+import ModalFormularioReparacion from '../../components/ModalFormularioReparacion';
 
 const Reparaciones = () => {
-
   const [reparaciones, loading] = useGetData('reparacion');
   const [searchTerm, setSearchTerm] = useState('');
   const [sortConfig, setSortConfig] = useState({
-    fecha_desde: {key: 'fecha_desde', direction: 'ascending'},
-    fecha_hasta: {key: 'fecha_hasta', direction: 'ascending'},
+    fecha_desde: { key: 'fecha_desde', direction: 'ascending' },
+    fecha_hasta: { key: 'fecha_hasta', direction: 'ascending' },
   });
+
   // Acá se define la estructura del objeto que vamos a estar trabajando en esta página
-  const props={
-    id_reparacion:'',
-    fecha_desde:'',
-    fecha_hasta:'',
-    comentarios:'',
-    patente:''
-}
+  const props = {
+    id_reparacion: '',
+    fecha_desde: '',
+    fecha_hasta: '',
+    comentarios: '',
+    patente: ''
+  };
+
   const sortedReparaciones = reparaciones.sort((a, b) => {
-    if (a[sortConfig.key] < b[sortConfig.key]) {
-      return sortConfig.direction === 'ascending' ? -1 : 1;
+    const key = sortConfig.key;
+    const direction = sortConfig.direction;
+
+    if (a[key] < b[key]) {
+      return direction === 'ascending' ? -1 : 1;
     }
-    if (a[sortConfig.key] > b[sortConfig.key]) {
-      return sortConfig.direction === 'ascending' ? 1 : -1;
+    if (a[key] > b[key]) {
+      return direction === 'ascending' ? 1 : -1;
     }
     return 0;
   });
+
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
-  
+
   const currentUrl = window.location.href;
   const partes = currentUrl.split("/");
-  const codigo = partes[partes.length - 1];
+  const codigo = partes[partes.length - 1].toLowerCase();
 
-  const filteredReparaciones = reparaciones.filter(reparacion => 
-
-    codigo === 'reparaciones' ? 
-    reparacion.patente.toLowerCase().includes(searchTerm):
-    reparacion.patente = codigo 
-
-  );
+  const filteredReparaciones = reparaciones.filter(reparacion => {
+    if (codigo === 'reparaciones') {
+      return reparacion.patente.toLowerCase().includes(searchTerm.toLowerCase());
+    } else {
+      return reparacion.patente.toLowerCase() === codigo;
+    }
+  });
 
   return (
     <>
-        <div className='table-responsive col-12 col-md-6 col-lg-9'>
-          <h1 className='text-left'>Reparaciones</h1>
-          <br />
-          <ModalFormularioReparacion element={"reparacion"} value={"Nueva reparación"} props={props} mode={'new'}/>
-          <br/><br/>
+      <div className='table-responsive col-12 col-md-6 col-lg-9'>
+        <h1 className='text-left'>Reparaciones</h1>
+        <br />
+        <ModalFormularioReparacion element={"reparacion"} value={"Nueva reparación"} props={props} mode={'new'} />
+        <br /><br />
 
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Buscar reparacion por patente"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Buscar reparacion por patente"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
 
-          <br/>
+        <br />
 
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                    <th scope="col">Comentarios</th>
-                    <th scope="col">Fecha desde
-                      <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
-                      <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
-                    </th>
-                    <th scope="col">Fecha hasta
-                      <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
-                      <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
-                    </th>
-                    <th scope="col">Patente</th>
-                    <th scope="col">Acciones</th>
-              </tr>
-            </thead>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Comentarios</th>
+              <th scope="col">Fecha desde
+                <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
+                <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
+              </th>
+              <th scope="col">Fecha hasta
+                <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
+                <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
+              </th>
+              <th scope="col">Patente</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
 
-            {loading && <Loading/>}
+          {loading && <Loading />}
 
-            <tbody>
-                {
-                  filteredReparaciones !== '' ? (
-                  filteredReparaciones.map((r) => {
-                    return (
-                      <tr key={r.id}>
-                        <td>{r.id}</td>
-                        <td>{r.comentarios}</td>
-                        <td>{r.fecha_desde}</td>
-                        <td>{r.fecha_hasta}</td>
-                        <td>{r.patente}</td>
-                        <EditDelete data={reparaciones} element={"reparacion"} id={r.id}/>
-                      </tr>
-                    )
-                  })
-                ):(
-                  reparaciones.map((r) => {
-                    return (
-                      <tr key={r.id}>
-                        <td>{r.id}</td>
-                        <td>{r.comentarios}</td>
-                        <td>{r.fecha_desde}</td>
-                        <td>{r.fecha_hasta}</td>
-                        <td>{r.patente}</td>
-                        <EditDelete data={reparaciones} element={"reparacion"} id={r.id}/>
-                      </tr>
-                    )
-                  })
-
-                )
-                }
-            </tbody>
-          </table>
-        </div>
+          <tbody>
+            {
+              filteredReparaciones.map((r) => (
+                <tr key={r.id}>
+                  <td>{r.id}</td>
+                  <td>{r.comentarios}</td>
+                  <td>{r.fecha_desde}</td>
+                  <td>{r.fecha_hasta}</td>
+                  <td>{r.patente}</td>
+                  <EditDelete data={reparaciones} element={"reparacion"} id={r.id} />
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default Reparaciones
+export default Reparaciones;
--- src/pages/Trabajos/Trabajos.jsx
+++ src/pages/Trabajos/Trabajos.jsx
@@ -1,169 +1,149 @@
-import {React, useState} from 'react'
+import React, { useState } from 'react';
 import { deleteData, useGetData } from '../../hooks';
-import {ModalFormularioTrabajo, Loading} from '../../components/'
+import { ModalFormularioTrabajo, Loading } from '../../components';
 
 const Trabajos = () => {
-    const [trabajos, loadingTrabajos] = useGetData('trabajo');
-    const [searchTerm, setSearchTerm] = useState('');
-    const [sortConfig, setSortConfig] = useState({
-        fecha_desde: {key: 'fecha_desde', direction: 'ascending'},
-        fecha_hasta: {key: 'fecha_hasta', direction: 'ascending'},
-      });
-    // Acá se define la estructura del objeto que vamos a estar trabajando en esta página
-    const props={
-        id_trabajo:'',
-        fecha_desde:'',
-        fecha_hasta:'',
-        kilometraje:0,
-        patente:'',
-        id_proyecto:0,
-        dni_conductor:'',
-        cuit_cliente:'',
+  const [trabajos, loadingTrabajos] = useGetData('trabajo');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortConfig, setSortConfig] = useState({
+    key: 'fecha_desde',
+    direction: 'ascending'
+  });
+
+  // Acá se define la estructura del objeto que vamos a estar trabajando en esta página
+  const props = {
+    id_trabajo: '',
+    fecha_desde: '',
+    fecha_hasta: '',
+    kilometraje: 0,
+    patente: '',
+    id_proyecto: 0,
+    dni_conductor: '',
+    cuit_cliente: '',
+  };
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const currentUrl = window.location.href;
+  const partes = currentUrl.split("/");
+  const codigo = partes[partes.length - 1].toLowerCase();
+
+  const filteredTrabajos = trabajos.filter(trabajo => {
+    if (codigo === 'trabajos') {
+      return (
+        trabajo.patente.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        trabajo.cliente.razon_social.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        trabajo.proyecto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      return trabajo.patente.toLowerCase() === codigo;
     }
+  });
 
-    const handleSearch = event => {
-        setSearchTerm(event.target.value);
-    };
+  const exportToCSV = (data, filename) => {
+    let csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += "ID,Patente,Fecha desde,Fecha hasta,Tipo trabajo,Cliente/Proyecto\n"; // Encabezados de las columnas
 
-    const currentUrl = window.location.href;
-    const partes = currentUrl.split("/");
-    const codigo = partes[partes.length - 1]
-    
+    data.forEach(trabajo => {
+      csvContent += `${trabajo.id_trabajo},${trabajo.patente},${trabajo.fecha_desde},${trabajo.fecha_hasta},${trabajo.cuit_cliente === '00000000000' ? "Propio" : "Alquiler"},${trabajo.cuit_cliente === '00000000000' ? "Proyecto: " + trabajo.proyecto.nombre : trabajo.cliente.razon_social}\n`;
+    });
 
-    const filteredTrabajos = trabajos.filter(trabajo =>
-        codigo === 'trabajos' ? 
-         trabajo.patente.toLowerCase().includes(searchTerm.toLowerCase())
-        || trabajo.cliente.razon_social.toLowerCase().includes(searchTerm.toLowerCase())
-        || trabajo.proyecto.nombre.toLowerCase().includes(searchTerm.toLowerCase()):
-        trabajo.patente = codigo             
-    );
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", filename);
+    document.body.appendChild(link); // Requerido para Firefox
 
-    const exportToCSV = (data, filename) => {
-        let csvContent = "data:text/csv;charset=utf-8,";
-        csvContent += "ID,Patente,Fecha desde,Fecha hasta,Tipo trabajo,Cliente/Proyecto\n"; // Encabezados de las columnas
-       
-        data.forEach(trabajo => {
-           csvContent += `${trabajo.id_trabajo},${trabajo.patente},${trabajo.fecha_desde},${trabajo.fecha_hasta},${trabajo.cuit_cliente === '00000000000' ? "Propio" : "Alquiler"},${trabajo.cuit_cliente === '00000000000' ? "Proyecto: " + trabajo.proyecto.nombre : trabajo.cliente.razon_social}\n`;
-        });
-       
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", filename);
-        document.body.appendChild(link); // Requerido para Firefox
-       
-        link.click(); // Esto descarga el archivo
-        document.body.removeChild(link); // Limpiar
-    };
+    link.click(); // Esto descarga el archivo
+    document.body.removeChild(link); // Limpiar
+  };
 
-    const deleteItem = async (id) => {
-        await deleteData('trabajo', id);
-        window.location.reload();
-        console.log(id)
+  const deleteItem = async (id) => {
+    await deleteData('trabajo', id);
+    window.location.reload();
+    console.log(id);
+  };
+
+  const sortedTrabajos = filteredTrabajos.sort((a, b) => {
+    const key = sortConfig.key;
+    const direction = sortConfig.direction;
+
+    if (a[key] < b[key]) {
+      return direction === 'ascending' ? -1 : 1;
     }
-    
-    
-  const sortedTrabajos = trabajos.sort((a, b) => {
-    if (a[sortConfig.key] < b[sortConfig.key]) {
-       return sortConfig.direction === 'ascending' ? -1 : 1;
-    }
-    if (a[sortConfig.key] > b[sortConfig.key]) {
-       return sortConfig.direction === 'ascending' ? 1 : -1;
+    if (a[key] > b[key]) {
+      return direction === 'ascending' ? 1 : -1;
     }
     return 0;
   });
 
-    return (
-        <div className='table-responsive col-12 col-md-6 col-lg-9'>
-            <h1 className='text-left'>Trabajos</h1>
-            <br />
+  return (
+    <div className='table-responsive col-12 col-md-6 col-lg-9'>
+      <h1 className='text-left'>Trabajos</h1>
+      <br />
 
-            <ModalFormularioTrabajo value={"Nuevo trabajo"} props={props} mode={'new'}/>
-            &nbsp;
-            <button className="btn btn-orange" onClick={() => exportToCSV(filteredTrabajos, 'trabajos.csv')}>
-                Exportar a CSV
-            </button>
-            <br/><br/>
+      <ModalFormularioTrabajo value={"Nuevo trabajo"} props={props} mode={'new'} />
+      &nbsp;
+      <button className="btn btn-orange" onClick={() => exportToCSV(sortedTrabajos, 'trabajos.csv')}>
+        Exportar a CSV
+      </button>
+      <br /><br />
 
-            <input
-                type="text"
-                className="form-control"
-                placeholder="Buscar trabajo por patente, cliente o proyecto"
-                value={searchTerm}
-                onChange={handleSearch}
-            />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Buscar trabajo por patente, cliente o proyecto"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
 
-            <br/>
+      <br />
 
-            <table className="table table-striped">
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Patente</th>
-                    <th scope="col">Fecha desde
-                    <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
-                    <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
-                    </th>
-                    <th scope="col">Fecha hasta
-                    <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
-                    <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
-                    </th>
-                    <th scope="col">Tipo trabajo</th>
-                    <th scope="col">Cliente/Proyecto</th> {/* Añade esta línea */}
-                    <th scope="col">Acciones</th>
-                </tr>
-                </thead>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Patente</th>
+            <th scope="col">Fecha desde
+              <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
+              <a onClick={() => setSortConfig({ key: 'fecha_desde', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
+            </th>
+            <th scope="col">Fecha hasta
+              <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'ascending' })}><i className="bi bi-arrow-up-short"></i></a>
+              <a onClick={() => setSortConfig({ key: 'fecha_hasta', direction: 'descending' })}><i className="bi bi-arrow-down-short"></i></a>
+            </th>
+            <th scope="col">Tipo trabajo</th>
+            <th scope="col">Cliente/Proyecto</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
 
-                {loadingTrabajos && <Loading/>}
+        {loadingTrabajos && <Loading />}
 
-                <tbody>
-                    {
-                    filteredTrabajos !== '' ? (
-
-                    filteredTrabajos.map((t) => {
-                        return (
-                            <tr key={t.id_trabajo}>
-                                <td>{t.id_trabajo}</td>
-                                <td>{t.patente}</td>
-                                <td>{t.fecha_desde}</td>
-                                <td>{t.fecha_hasta}</td>
-                                <td>{t.cuit_cliente === '00000000000' ? "Propio" : "Alquiler"}</td>
-                                <td>{t.cuit_cliente === '00000000000' ? "Proyecto: " + t.proyecto.nombre : t.cliente.razon_social}</td> 
-                                <td>
-                                    <ModalFormularioTrabajo value={<i class="bi bi-pencil"></i>} props={trabajos.find((objeto) => Object.values(objeto)[0] === t.id_trabajo)} mode={'update'} id={t.id_trabajo}/>
-                                    &ensp;
-                                    <button className='btn btn-danger' onClick={() => {deleteItem(t.id_trabajo)}}><i class="bi bi-trash3"></i></button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                    ) : (
-                    trabajos.map((t) => {
-                        return (
-                            <tr key={t.id_trabajo}>
-                                <td>{t.id_trabajo}</td>
-                                <td>{t.patente}</td>
-                                <td>{t.fecha_desde}</td>
-                                <td>{t.fecha_hasta}</td>
-                                <td>{t.cuit_cliente === '00000000000' ? "Propio" : "Alquiler"}</td>
-                                <td>{t.cuit_cliente === '00000000000' ? "Proyecto: " + t.proyecto.nombre : t.cliente.razon_social}</td> 
-                                <td>
-                                    <ModalFormularioTrabajo value={<i class="bi bi-pencil"></i>} props={trabajos.find((objeto) => Object.values(objeto)[0] === t.id_trabajo)} mode={'update'} id={t.id_trabajo}/>
-                                    &ensp;
-                                    <button className='btn btn-danger' onClick={() => {deleteItem(t.id_trabajo)}}><i class="bi bi-trash3"></i></button>
-                                </td>
-                            </tr>
-                        )
-                    }))
-
-
-
-
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+        <tbody>
+          {
+            sortedTrabajos.map((t) => (
+              <tr key={t.id_trabajo}>
+                <td>{t.id_trabajo}</td>
+                <td>{t.patente}</td>
+                <td>{t.fecha_desde}</td>
+                <td>{t.fecha_hasta}</td>
+                <td>{t.cuit_cliente === '00000000000' ? "Propio" : "Alquiler"}</td>
+                <td>{t.cuit_cliente === '00000000000' ? "Proyecto: " + t.proyecto.nombre : t.cliente.razon_social}</td>
+                <td>
+                  <ModalFormularioTrabajo value={<i className="bi bi-pencil"></i>} props={trabajos.find((objeto) => Object.values(objeto)[0] === t.id_trabajo)} mode={'update'} id={t.id_trabajo} />
+                  &ensp;
+                  <button className='btn btn-danger' onClick={() => { deleteItem(t.id_trabajo) }}><i className="bi bi-trash3"></i></button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Trabajos
+export default Trabajos;
