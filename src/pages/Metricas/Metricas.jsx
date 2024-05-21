b4d4b3b59a9f5bbe340eb423022e9b0699e26a84
Germán Tano <germantano88@gmail.com>
2024-05-21 10:04:37 -0300

    agrando tablero métricas

--- src/pages/Metricas/Metricas.jsx
+++ src/pages/Metricas/Metricas.jsx
@@ -3,12 +3,10 @@ import {React} from 'react'
 const Metricas = () => {
 
   return (
-    <>      
-        <div className='table-responsive col-12 col-md-6 col-lg-9'>
-          <h1 className='text-left' style={{color: '#47525E'}}>Métricas de uso</h1>
-            <iframe title="metricas" width="1020" height="600" src="https://app.powerbi.com/reportEmbed?reportId=a0e09203-b40d-4989-9878-808447aedbb0&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
-        </div>
-    </>
+      <div className='col-12 col-md-6 col-lg-9'>
+        <h1 className='text-left' style={{color: '#47525E'}}>Métricas de uso</h1>
+          <iframe title="metricas" width="1040" height="640" src="https://app.powerbi.com/reportEmbed?reportId=a0e09203-b40d-4989-9878-808447aedbb0&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
+      </div>
     )
     
 }
